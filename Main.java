c9778c310062aa5e053d28846f3bec2ff6074a40
SRINITHYA04 <srinithyathiyagaraj@gmail.com>
2024-01-12 17:02:31 +0530

    commmit changes

--- Main.java
+++ Main.java
@@ -63,7 +63,7 @@ public class Main {
     public static void printBoard(char[][] board){
          for(int row=0;row<board.length;row++){
             for(int col=0;col<board[row].length;col++){
-                System.out.print(board[row][col]+" ");
+                System.out.print(board[row][col]+" | ");
         }
         System.out.println();
     }
